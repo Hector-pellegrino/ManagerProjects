214b565557ec0a73c14a2d91e0d81ae0c6d3ea5c
Hector Pellegrino <fabropelegrinih@gmail.com>
2025-09-17 22:48:25 -0300

    Colocando o .env para o uso da api

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,4 +1,5 @@
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000'
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+
 
 import styles from './Projects.module.css'
 
